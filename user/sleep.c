c9308ef7cda37bdcc1cf300899837ebc28d16723
lfhuang0701 <lfhuang2022@163.com>
2024-04-17 21:46:02 +0800

    完成第一个作业

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2){
+        fprintf(1, "must 1 argument for sleep!");
+        exit(1);
+    }
+    int sleepNum = atoi(argv[1]);
+    printf("(nothing happen for a little while)\n");
+    sleep(sleepNum);
+    //printf("sleep %d success\n", &sleepNum);
+    exit(0);
+
+}
