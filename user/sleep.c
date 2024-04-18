594887cc8e26034798fb4b1dcf1d4714f0173051
lfhuang0701 <lfhuang2022@163.com>
2024-04-18 17:14:16 +0800

    test
    Signed-off-by: lfhuang0701 <lfhuang2022@163.com>

--- user/sleep.c
+++ user/sleep.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     int sleepNum = atoi(argv[1]);
     printf("(nothing happen for a little while)\n");
     sleep(sleepNum);
-    //printf("sleep %d success\n", &sleepNum);
+    printf("sleep %d success\n", &sleepNum);
     exit(0);
 
 }
